098c2d1992d7b268e60be08223b00a742551794c
L.V.D.E. Vogelsmeier <l.v.d.e.vogelsmeier@tilburguniversity.edu>
2021-03-18 16:32:34 +0100

    added example data

---
+++ R/8ESMdata.R
@@ -0,0 +1,33 @@
+#' Example ESM dataset
+#'
+#' The variables are as follows:
+#'
+#' @format A data frame with 4776 rows and 23 variables:
+#' \itemize{
+#'   \item id: 
+#'   \item deltaT:
+#'   \item negativeEvent: 
+#'   \item intervention: 
+#'   \item interactions: 
+#'   \item Interested:
+#'   \item Joyful:
+#'   \item Determined:
+#'   \item Calm:
+#'   \item Lively:
+#'   \item Enthusiastic:
+#'   \item Relaxed:
+#'   \item Cheerful:
+#'   \item Content:
+#'   \item Energetic:
+#'   \item Upset:
+#'   \item Gloomy:
+#'   \item Sluggish:
+#'   \item Anxious:
+#'   \item Bored:
+#'   \item Irritated:
+#'   \item Nervous:
+#'   \item Listless:
+#' }
+"ESM"
+
+
