e94185f9e32616908894322fffc04f13e070e5ee
LeonieVm <l.v.d.e.vogelsmeier@tilburguniversity.edu>
2022-03-30 18:04:50 +0200

    adjusted invariance function

--- R/7Step3Invariance.r
+++ R/7Step3Invariance.r
@@ -7,39 +7,41 @@
 #'
 #'
 #'
-#' @param object  Output from the main function step3() (must be of class lmfa_step3).
+#' @param model  Output from the main function step3() (must be of class lmfa_step3).
 #' @param identifier The name of the column containing the subject identifiers (must be a single character).
 #
 #'
 #'
 #' @examples
 #' \dontrun{
-#' invariance(object, identifier)
+#' invariance(model, identifier)
 #' }
 #' @export
 
 
-invariance <- function(object, identifier){
-        if(missing(object)) stop("argument data is missing, with no default")
+invariance <- function(model, identifier){
+        if(missing(model)) stop("argument data is missing, with no default")
         if(missing(identifier)) stop("argument identifier is missing, with no default")
-        if(class(object)!="lmfa_step3") stop("model must be of class lmfa_step3")
+        if(class(model)!="lmfa_step3") stop("model must be of class lmfa_step3")
 
 
-        dataInvariance <- object$data
+        dataInvariance <- model$data
         invariance_time <- c()
         statemembership <- c()
         subjects <- unique(dataInvariance[,identifier])
         for(i in 1:length(subjects)){
         subject_number <- unique(dataInvariance[,identifier])[i]
         
-        if(length(unique(dataInvariance[dataInvariance[,identifier]==subject_number,"Modal"]))==1){
+        #if within perosn invariance holds...
+        if(length(unique(dataInvariance[dataInvariance[,identifier]==subject_number,"Modal"]))!=1){
             invariance_time <- c(invariance_time,subject_number)
-            statemembership <- c(statemembership,unique(dataInvariance[dataInvariance[,identifier]==subject_number,"Modal"]))
+            statemembership <- c(statemembership,unique(dataInvariance[dataInvariance[,identifier]==subject_number,"Modal"])[1])
         }
         }
+        #if it holds for anyone...
         if(!is.null(statemembership)){
             statemembership <- as.matrix(statemembership,ncol=1)
-        rownames(statemembership) <- invariance_time
+            rownames(statemembership) <- invariance_time
 
 
         invariance_subjects <- list()
@@ -49,10 +51,34 @@ invariance <- function(object, identifier){
 
         names(invariance_subjects) <- paste("S",sort(unique(statemembership)),sep="")
 
-        print(invariance_subjects)
+            for(i in 1: length(invariance_subjects)){
+                cat(names(invariance_subjects)[i])
+                cat("\n")
+                
+                cat(invariance_subjects[[i]])
+                cat("\n")
+                cat("\n")
+            }
+
+        #if it does not hold for anyone...
         }else{
-            cat(paste("There are no individuals for whom invariance over time holds.","\n"))
-            cat("\n")
+
+        x_n_state <- 1:model$n_state
+        invariance_subjects <- list()
+        for (i in 1:length(x_n_state)) {
+        invariance_subjects[[x_n_state[i]]] <- NA
+        }
+
+        names(invariance_subjects) <- paste("S",1:model$n_state,sep="")
+            for(i in 1: length(invariance_subjects)){
+                cat(names(invariance_subjects)[i])
+                cat("\n")
+                
+                cat(invariance_subjects[[i]])
+                cat("\n")
+                cat("\n")
+            }
+
         }
 
 }
