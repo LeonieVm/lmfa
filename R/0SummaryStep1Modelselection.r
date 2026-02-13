a72ca840ac643c2b9f9438e60306049c99e932ef
LeonieVm <l.v.d.e.vogelsmeier@tilburguniversity.edu>
2026-02-13 10:19:54 +0100

    adjusted lmfa step 1 model selection for comparison when only n_states but not n_factors varies

--- R/0SummaryStep1Modelselection.r
+++ R/0SummaryStep1Modelselection.r
@@ -35,29 +35,38 @@ modelcomparison <-modelcomparison[order(modelcomparison[,"BIC"]),]
 
 modelcomparison2 <- c()
 unistates <- unique(modelcomparison[,"n_state"])
+
 for(i in 1:length(unistates)){
   modeli <- modelcomparison[modelcomparison[,"n_state"]==unistates[i],]
-  modeli <- modeli[order(modeli[,"n_par"]),]
-  local_max <- c(0)
-  for(j in 2:nrow(modeli)){
-    if(modeli[j,"n_par"] != modeli[j-1,"n_par"]){
-      local_max <- c(local_max,sum((modeli[j,"LL"]-modeli[j-1,"LL"])<0))
-    }else{
-      if((j-2)<1){
-        local_max <- c(local_max,0)
+  if (!is.null(nrow(modeli))) {
+    modeli <- modeli[order(modeli[,"n_par"]),]
+    local_max <- c(0)
+    for(j in 2:nrow(modeli)){
+      if(modeli[j,"n_par"] != modeli[j-1,"n_par"]){
+        local_max <- c(local_max,sum((modeli[j,"LL"]-modeli[j-1,"LL"])<0))
       }else{
-        local_max <- c(local_max,sum((modeli[j,"LL"]-modeli[j-2,"LL"])<0))
+        if((j-2)<1){
+          local_max <- c(local_max,0)
+        }else{
+          local_max <- c(local_max,sum((modeli[j,"LL"]-modeli[j-2,"LL"])<0))
+        }
       }
     }
+    modeli <- cbind(modeli,local_max)
+    modelcomparison2 <- rbind(modelcomparison2,modeli)
+  }else{
+    modelcomparison2 <- modelcomparison
   }
-  modeli <- cbind(modeli,local_max)
-  modelcomparison2 <- rbind(modelcomparison2,modeli)
 }
 
-modelcomparison2 <-modelcomparison2[order(modelcomparison2[,"BIC"]),]
-
-objecModelselection <- (modelcomparison2[,c(1:4,7)])
-print(objecModelselection[,-5])
+if(ncol(modelcomparison2)==7){
+  modelcomparison2 <-modelcomparison2[order(modelcomparison2[,"BIC"]),]
+  
+  objecModelselection <- (modelcomparison2[,c(1:4,7)])
+  print(objecModelselection[,-5])
+}else{
+  print(modelcomparison[,-c(5,6)])
+}
 cat("\n")
 cat(paste("Note: When re-estimating models that did not converge, the"),"\n")
 cat(paste("   number of maximum iterations should be increased."),"\n")
